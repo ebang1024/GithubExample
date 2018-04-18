2aef1295abee5854a4363bc956bdb91be7f2b542
Ethan Bang <ethanbang@jh-mba158-01.d214.org>
2018-04-18 15:03:32 -0500

    added view controller

---
+++ GithubExample/NewViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NewViewController.swift
+//  GithubExample
+//
+//  Created by Ethan Bang on 4/18/18.
+//  Copyright © 2018 Ethan Bang. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIView {
+
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
